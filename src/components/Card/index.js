7abbb185fbeb72c7d6fefc7e9dbd6e27ed21a8a0
Sidney Pimentel Eleutério <sidneyspe@gmail.com>
2019-11-17 19:32:05 -0300

    added lint-staged

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,35 +1,60 @@
-import React, { useRef } from 'react';
+import React, { useRef, useContext } from "react";
+import PropTypes from "prop-types";
 
-import { useDrag, useDrop } from 'react-dnd';
+import { useDrag, useDrop } from "react-dnd";
 
-import { Container, Label } from './styles';
+import BoardContext from "../Board/context";
 
-export default function Card({ data, index }) {
+import { Container, Label } from "./styles";
+
+export default function Card({ data, index, listIndex }) {
   const ref = useRef();
+  const { move } = useContext(BoardContext);
 
   const [{ isDragging }, dragRef] = useDrag({
     item: {
-      type: 'CARD', index,
+      type: "CARD",
+      index,
+      listIndex
     },
-    collect: (monitor) => ({
-      isDragging: monitor.isDragging(),
-
-    }),
+    collect: monitor => ({
+      isDragging: monitor.isDragging()
+    })
   });
 
   const [, dropRef] = useDrop({
-    accept: 'CARD',
+    accept: "CARD",
     hover(item, monitor) {
+      const draggedListIndex = item.listIndex;
+      const targetListIndex = listIndex;
+
       const draggedIndex = item.index;
       const targetIndex = index;
 
-      if (draggedIndex === targetIndex) {
+      if (
+        draggedIndex === targetIndex &&
+        draggedListIndex === targetListIndex
+      ) {
         return true;
       }
 
       const targetSize = ref.current.getBoundingClientRect();
       const targetCenter = (targetSize.bottom - targetSize.top) / 2;
-    },
+
+      const draggedOffset = monitor.getClientOffset();
+      const draggedTop = draggedOffset.y - targetSize.top;
+      if (draggedIndex < targetIndex && draggedTop < targetCenter) {
+        return true;
+      }
+      if (draggedIndex > targetIndex && draggedTop > targetCenter) {
+        return true;
+      }
+
+      move(draggedListIndex, targetListIndex, draggedIndex, targetIndex);
+      item.index = targetIndex;
+      item.listIndex = targetListIndex;
+      return true;
+    }
   });
 
   dragRef(dropRef(ref));
@@ -37,12 +62,24 @@ export default function Card({ data, index }) {
   return (
     <Container ref={ref} isDragging={isDragging}>
       <header>
-        {data.labels.map((label) => <Label key={label} color={label} />)}
+        {data.labels.map(label => (
+          <Label key={label} color={label} />
+        ))}
       </header>
-      <p>
-        {data.content}
-      </p>
+      <p>{data.content}</p>
       {data.user && <img src={data.user} alt="" />}
     </Container>
   );
 }
+
+Card.propTypes = {
+  data: PropTypes.oneOfType([PropTypes.object, PropTypes.array]),
+  index: PropTypes.number,
+  listIndex: PropTypes.number
+};
+
+Card.defaultProps = {
+  data: {},
+  index: 0,
+  listIndex: 0
+};
--- src/components/List/index.js
+++ src/components/List/index.js
@@ -1,27 +1,38 @@
-import React from 'react';
+import React from "react";
+import PropTypes from "prop-types";
 
-import { MdAdd } from 'react-icons/md';
-import Card from '../Card';
+import { MdAdd } from "react-icons/md";
+import Card from "../Card";
 
-import { Container } from './styles';
+import { Container } from "./styles";
 
-export default function List({ data }) {
+export default function List({ data, index: listIndex }) {
   return (
     <Container done={data.done}>
       <header>
-        <h2>
-          {data.title}
-        </h2>
+        <h2>{data.title}</h2>
         {data.creatable && (
-        <button type="button">
-          <MdAdd size={24} color="#fff" />
-        </button>
+          <button type="button">
+            <MdAdd size={24} color="#fff" />
+          </button>
         )}
       </header>
 
       <ul>
-        { data.cards.map((card, index) => <Card key={card.id} index={index} data={card} />)}
+        {data.cards.map((card, index) => (
+          <Card key={card.id} listIndex={listIndex} index={index} data={card} />
+        ))}
       </ul>
     </Container>
   );
 }
+
+List.propTypes = {
+  data: PropTypes.oneOfType([PropTypes.object, PropTypes.array]),
+  index: PropTypes.number
+};
+
+List.defaultProps = {
+  data: {},
+  index: 0
+};
